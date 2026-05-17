6ac3f4f6b9035ba75bb2dd68aa6fdfb6aebe1c23
Cédric CARRÉE <beg0@free.fr>
2026-05-17 17:38:23 +0200

    feat!(selafin): First working impl & parser for geometry

---
+++ src/storage.rs
@@ -0,0 +1,13 @@
+//! # Data storage in Free-Surface
+//!
+//! This module gather the tools to read or write persistent data.
+//!
+//! It includes:
+//!
+//! - file system
+//! - data base
+//! - ...
+//!
+//! **Note**: Config files are managed in [config]
+
+pub mod selafin;
