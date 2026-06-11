a2a3774693386ee356da36087af9759f370df09e
Cédric CARRÉE <beg0@free.fr>
2026-06-12 00:02:23 +0200

    feat: introduce way to easily select ConfigViewer

--- src/aui/configviewer.rs
+++ src/aui/configviewer.rs
@@ -2,6 +2,9 @@
 //!
 //! e.g. display anything with key=value pairs, optionally organized in sections
 
+use anstream::stream::{AsLockedWrite, RawStream};
+use anstream::AutoStream;
+use clap::ColorChoice;
 use serde_json::value::Value as JsonValue;
 
 mod damocles;
@@ -32,4 +35,35 @@ pub trait ConfigViewer {
 
     /// Terminate the display
     fn finish(&mut self) {}
+}
+
+pub enum ConfigViewerOptions {
+    Damocles,
+    Human { color: ColorChoice },
+    Json { pretty: bool },
+    Machine,
+}
+
+/// Factory to create one of the supported format
+pub fn create_config_viewer<W: RawStream + AsLockedWrite + 'static>(
+    out: W,
+    options: ConfigViewerOptions,
+) -> Box<dyn ConfigViewer> {
+    // Dispatch to the right rendrer
+    match options {
+        ConfigViewerOptions::Damocles => Box::new(DamoclesConfigViewer::new(out)),
+        ConfigViewerOptions::Human { color } => {
+            // AutoStream strips ANSI codes automatically when the output is
+            // not a TTY or when NO_COLOR / --no-color is set.
+            let stream = match color {
+                ColorChoice::Always => AutoStream::always(out),
+                ColorChoice::Never => AutoStream::never(out),
+                ColorChoice::Auto => AutoStream::auto(out),
+            };
+
+            Box::new(HumanConfigViewer::new(stream))
+        }
+        ConfigViewerOptions::Json { pretty } => Box::new(JsonConfigViewer::new(out, pretty)),
+        ConfigViewerOptions::Machine => Box::new(PorcelainConfigViewer::new(out)),
+    }
 }
