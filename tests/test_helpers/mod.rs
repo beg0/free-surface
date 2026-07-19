8877f40ed8a88be29a68c1472b7a1220135e85bf
Cédric CARRÉE <beg0@free.fr>
2026-07-19 14:20:17 +0200

    feat(tests): add helper to read file line by line

--- tests/test_helpers/mod.rs
+++ tests/test_helpers/mod.rs
@@ -1,6 +1,8 @@
 //! # Test helpers
 //!
 //! Collection of helpers to be used in integration tests.
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::path::{Path, PathBuf};
 
 mod telemac_sources;
@@ -21,3 +23,25 @@ pub fn fixture(name: &str) -> PathBuf {
 
 #[allow(dead_code)]
 pub use telemac_sources::telemac_file;
+
+/// Read a file line by line
+///
+/// # Example
+///
+/// ```no_run
+///
+/// let lines = read_lines("my_file.txt").except("Can't read file");
+///
+/// for line in lines.map_while(Result::ok) {
+///   println!(line);
+/// }
+///
+/// ```
+#[allow(dead_code)]
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
