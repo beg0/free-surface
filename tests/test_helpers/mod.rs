b5c19f8fc4ce7c11eb36dfa4b333aff3cd747c6e
Cédric CARRÉE <beg0@free.fr>
2026-08-15 15:38:01 +0200

    feat(tests): Add predicates to ease multiline comparaison

--- tests/test_helpers/mod.rs
+++ tests/test_helpers/mod.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::{Path, PathBuf};
 
+pub mod predicates;
 mod telemac_sources;
 
 /// Get location of the (test) cache directory
@@ -44,4 +45,22 @@ where
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+/// Normalize end of lines
+///
+/// Input line may end with \n (unix style) or \r\n (dos style)
+/// Output every line end with \n (unix style)
+///
+/// Also trim end of line for tailing whitespaces to ease comparison
+#[allow(dead_code)]
+pub fn normalize_eol(txt: &str) -> String {
+    let mut output = String::with_capacity(txt.len());
+
+    for line in txt.lines() {
+        output.push_str(line.trim_end());
+        output.push('\n')
+    }
+
+    output
 }
