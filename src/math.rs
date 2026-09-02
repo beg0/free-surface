c1a030b11be8d2d88ce78d62b222f11d3f5e80dd
Cédric CARRÉE <beg0@free.fr>
2026-09-02 22:20:54 +0200

    feat(math): introduce Point2f

--- src/math.rs
+++ src/math.rs
@@ -5,6 +5,48 @@ use std::fmt;
 pub mod backend;
 pub mod bief;
 
+/// Coordinate of a 2D points
+#[derive(Clone)]
+pub struct Point2f {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl fmt::Display for Point2f {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl fmt::Debug for Point2f {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl Point2f {
+    /// Compute the distance from another point
+    pub fn distance(&self, other: &Point2f) -> f64 {
+        self.distance2(other).sqrt()
+    }
+
+    /// Compute the square of the distance from another point
+    pub fn distance2(&self, other: &Point2f) -> f64 {
+        let delta_x = self.x - other.x;
+        let delta_y = self.y - other.y;
+
+        delta_x * delta_x + delta_y * delta_y
+    }
+
+    pub fn as_point3f(&self) -> Point3f {
+        Point3f {
+            x: self.x,
+            y: self.y,
+            z: 0.0,
+        }
+    }
+}
+
 /// Coordinate of a 3D points
 #[derive(Clone)]
 pub struct Point3f {
